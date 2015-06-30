8ffd6558d89a145e93787b04f1682308dcc1d842
yasin turkoglu <yasinturkoglu@yasin-MacBook-Pro.local>
2015-06-30 14:00:36 +0300

    Initial Commit

---
+++ withoutStoryboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  withoutStoryboard
+//
+//  Created by yasin turkoglu on 30.06.2015.
+//  Copyright (c) 2015 yasin turkoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
